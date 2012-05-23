52bc267015038de874005e3eb124f9de8b98103b
William E <testorati@gmail.com>
2012-05-23 09:53:43 -0400

    - Added support for multiple clocks runnig at the same time - Added new 'reset' action - Modified 'test.html' to showcase multiple clocks

--- js/jquery.timeup-0.0.1.js
+++ js/jquery.timeup-0.0.1.js
@@ -23,67 +23,96 @@
 */
 /*
 	
-	timeup - A count up timer (h:m:s)
+	timeup - A simple count up timer (h:m:s)
 
 	usage:
-		@click: $("#container").timeup('start');
-		@click: $("#container").timeup('stop');
-	note:
-		timeup('stop') will reset the counter (for  now)
-	
+		@click: $("#element").timeup('start','id');
+		@click: $(".element").timeup('start','class');
 */
 (function($)
 {
-	// For now let's put this here
-	var	timerHandle = 0;
-	var	timerOn = false;
-
-	$.fn.timeup = function(action)
+	// Let's create small db to keep track of multiple clocks
+	// We must do this so we can have multiple clocks running
+	var	Clocks = new Object();
+		Clocks.db = new Array();
+	
+	// We need a way to get the records off the db
+	Clocks.getClocks = function(ClockID)
 	{
-		var	seconds = 0;
-		var	minutes = 0;
-		var	hours = 0;
-		var	tagType = this.get(0).tagName.toLowerCase();
-		var	timerStr = '';
-		var	tu = this;
+		// If we already have clockID on db, return it
+		if (Clocks.db[ClockID] != undefined)
+			return Clocks.db[ClockID];
 
-		if (action == 'start')
+		// We don't have ClockID on the db, create it and return it
+		return (Clocks.db[ClockID] = 
 		{
-			if (timerOn == false)
-			{
-				timerHandle = setInterval(timer, 1000);
-				timerOn = true;
-			}
-		}
+			Handle: 0,
+			On: false,
+			Seconds: 0,
+			Minutes: 0,
+			Hours: 0,
+			Clock: ClockID
+		});
+	};
+	
+	// Let's insert the plugin into jQuery.fn object
+	$.fn.timeup = function(action, clockIdentifier)
+	{
+		// Get current jquery object
+		var	tuObj = this;
 		
-		if (action == 'stop')
+		// Get clock or start with a fresh one
+		var	currentClock = Clocks.getClocks(tuObj.attr(clockIdentifier));
+
+		// Determine tag type
+		var	tagType = this.get(0).tagName.toLowerCase();
+		
+		// What action to take
+		switch(action)
 		{
-			clearInterval(timerHandle);
-			timerOn = false;
+			case 'start':
+				currentClock.Handle = setInterval(timer, 1000);
+				currentClock.On = true;
+				break;
+			case 'stop':
+				clearInterval(currentClock.Handle);
+				currentClock.On = false;
+				break;
+			case 'reset':
+				currentClock.Seconds = 0;
+				currentClock.Minutes = 0;
+				currentClock.Hours = 0;
+				break;
 		}
 
+
+		// The actual timer function
 		function timer()
 		{
-			seconds++;
+			// Time string
+			var	timeStr = '';
 
-			if (seconds > 59)
+			currentClock.Seconds++;
+
+			if (currentClock.Seconds > 59)
 			{
-				seconds = 0;
-				minutes += 1;
+				currentClock.Seconds = 0;
+				currentClock.Minutes += 1;
 			}
-			if (minutes > 59)
+
+			if (currentClock.Minutes > 59)
 			{
-				minutes = 0;
-				hours += 1;
+				currentClock.Minutes = 0;
+				currentClock.Hours += 1;
 			}
-			
-			timerStr = hours + ':' + minutes + ':' + seconds;
+
+			// Display
+			timeStr = currentClock.Hours + ':' + currentClock.Minutes + ':' + currentClock.Seconds;
 
 			if ((tagType == 'input') || (tagType == 'textarea'))
-				tu.val(timerStr);
+				tuObj.val(timeStr);
 			else
-				tu.html(timerStr);
-
+				tuObj.text(timeStr);
 		};
 	};
 })(jQuery);
